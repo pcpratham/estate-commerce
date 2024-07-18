e01f3e803f1eebbe0755fd0caa2ba368f0ed5df1
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-07-18 23:05:23 +0530

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignOut from './pages/SignOut'
 function App() {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-out" element={<SignOut />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
