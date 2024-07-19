a8462c9501833449956c556b9c54ee35cb2c6e0d
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-07-19 13:41:21 +0530

    DB connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    
+    console.log("Connected to MongoDB!!");
+})
+.catch((err)=>{
+    console.log("Error connecting to MongoDB!!",err);
+});
+
+
 
 const app = express();
 
 app.listen(3000,()=>{
     console.log('Server running at port 3000!!');
 })
+
+
+
+
