7e851dc4d64dad970abc509068c17231805a4162
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-07-19 18:37:38 +0530

    git signup api route

--- server/index.js
+++ server/index.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
-const userRouter = require('./routes/user.route.js');
+const userRouter = require("./routes/user.route.js");
+const authRouter = require("./routes/auth.route.js");
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    
+// ----- Main code -----
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MongoDB!!");
-})
-.catch((err)=>{
-    console.log("Error connecting to MongoDB!!",err);
-});
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB!!", err);
+  });
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log('Server running at port 3000!!');
-})
+app.listen(3000, () => {
+  console.log("Server running at port 3000!!");
+});
 
-app.use('/api/user',userRouter);
-
-
+// ----- Routes -----
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
