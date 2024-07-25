432038c25e885da8264498899d3e6c3d7eb189af
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-07-25 17:53:56 +0530

    profile page UI

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
 
 function Profile() {
+  const {currentUser} = useSelector(state => state.user);
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto '>
+      <h1 className='text-3xl font-bold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4 '>
+        <img src={currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 ' />
+        <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg  ' />
+        <input type='text' placeholder='email' id='email' className='border p-3 rounded-lg  ' />
+        <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg  '/>
+        <button className='bg-slate-700 rounded-lg text-white uppercase p-3 hover:opacity-95 '>Update Details</button>
+
+      </form>
+      <div className='flex justify-between mt-5 '>
+        <span className='text-red-700 cursor-pointer hover:font-bold transition-all'>Delete Account</span>
+        <span className='text-red-700 hover:font-bold transition-all cursor-pointer'>Sign out</span>
+
+      </div>
+    </div>
   )
 }
 
